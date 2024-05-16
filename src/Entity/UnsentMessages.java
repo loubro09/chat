260ab92ca21d00e4d528fffb811c4c44e2736bdf
utbkenalt <utbkenalt@gmail.com>
2024-05-16 11:46:02 +0200

    Java Docs

--- src/Entity/UnsentMessages.java
+++ src/Entity/UnsentMessages.java
@@ -3,21 +3,34 @@ package Entity;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Class used to save the message that we're sent to offline users.
+ * Saving the messages using a HashMap until the receiver user gets online.
+ */
 public class UnsentMessages {
-
     private HashMap<User, ArrayList<Message>> messages = new HashMap<>();
-    // egna tillägg
+
+    /**
+     * A method to retrieve the ArrayList for the user, or create a new one if it doesn't exist.
+     * Adds the message to the ArrayList.
+     * Puts the ArrayList back into the HashMap.
+     * @param user The receiver user
+     * @param message
+     */
     public synchronized void put(User user, Message message) {
-        // Retrieve the ArrayList for the user, or create a new one if it doesn't exist
         ArrayList<Message> userMessages = messages.getOrDefault(user, new ArrayList<>());
-        // Add the message to the ArrayList
         userMessages.add(message);
-        // Put the ArrayList back into the map
         messages.put(user, userMessages);
     }
+
+    /**
+     * Getter for the ArrayList for a specific user.
+     * @param user the user object.
+     * @return returns the ArrayList for the user object.
+     */
     public synchronized ArrayList<Message> get(User user) {
-        // Retrieve and return the ArrayList for the user
         return messages.get(user);
     }
+
 // fler synchronized-metoder som behövs
 }
--- src/Entity/User.java
+++ src/Entity/User.java
@@ -3,33 +3,64 @@ package Entity;
 import java.io.Serializable;
 import java.util.*;
 
+/**
+ * The user class used to make user objects.
+ * implementing Serializable so that the instances can be serialized, to let them
+ * be converted into a stream of bytes that can be stored in a file or sent over the network.
+ */
 public class User implements Serializable {
 
     private String userName;
     private boolean online;
     private List<User> friendList = new ArrayList<>();
 
-    public void setFriendList(List<User> friendList) {
-        this.friendList = friendList;
-    }
-
+    /**
+     * Constructor used to create a user object.
+     * @param userName The username for the user object being created.
+     */
     public User(String userName) {
         this.userName = userName;
         this.online = false;
     }
-
+    /**
+     * Setter to create friends list for a user.
+     * @param friendList the list of friends for a specific user.
+     */
+    public void setFriendList(List<User> friendList) {
+        this.friendList = friendList;
+    }
+    /**
+     * Setter to change the online (status) boolean for the user.
+     * @param online Boolean to track if th user is online or offline.
+     */
     public void setOnline(boolean online) {
         this.online = online;
     }
+    /**
+     * Getter for the online boolean.
+     * @return oline boolean.
+     */
     public boolean getOnline() {
         return online;
     }
+    /**
+     * Getter for the hashcode to get the hash code for a specific username.
+     * @return hashCode int.
+     */
     public int hashCode() {
         return userName.hashCode();
     }
+    /**
+     * Getter for the username of the user object.
+     * @return userName String.
+     */
     public String getUserName() {
         return userName;
     }
+    /**
+     * Getter for the friends list.
+     * @return friendList List.
+     */
     public List<User> getFriendList() {
         return friendList;
     }
